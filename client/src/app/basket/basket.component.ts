2f038af18ee680c791dfa6dcc35493be3ae8347a
Assaf43 <himantz43@gmail.com>
2023-04-18 10:10:55 +0300

    client - checkout

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -7,18 +7,14 @@ import { BasketService } from './basket.service';
   templateUrl: './basket.component.html',
   styleUrls: ['./basket.component.scss'],
 })
-export class BasketComponent implements OnInit {
+export class BasketComponent {
   constructor(public basketService: BasketService) {}
 
   incrementQuantity(item: BasketItem) {
     this.basketService.addItemToBasket(item);
   }
 
-  removeItem(id: number, quantity: number) {
-    this.basketService.removeItemFromBasket(id, quantity);
-  }
-
-  ngOnInit(): void {
-    console.log('In BasketComponent');
+  removeItem(event: { id: number; quantity: number }) {
+    this.basketService.removeItemFromBasket(event.id, event.quantity);
   }
 }
